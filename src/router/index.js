ff918320f3ed360d76f653cafabfc51aeb8ad315
BrooksPMA <68705027+BrooksPMA@users.noreply.github.com>
2022-12-13 14:13:17 +0300

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue';
 import Distribute from '../components/Distribute.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
